e757876eba3e5911712f8bb31837d9559591019e
Ryan Warner <ryan.warner.us@gmail.com>
2022-07-19 18:19:27 +0200

    Basic API framework

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/api/v1/getProfile")
-def getProfile():
-    return {"string": "Hello world!"}
+@app.route("/api/v1/auth/login", methods=["POST"])
+def login():
+    requestData = request.get_json()
+    testData = requestData["temp"]
+    return {"token": testData}
 
-@app.route("/api/v1/getProfile")
-def getProfile():
-    return {"string": "Hello world!"}
+@app.route("/api/v1/auth/logout", methods=["POST"])
+def logout():
+    return {"string": "Logging out user."}
+
+@app.route("/api/v1/user", methods=["GET", "POST"])
+def user():
+    if request.method == "GET":
+        return {
+            "name": "Ryan Warner",
+            "username": "ryan-warner",
+            "email": "ryan.warner@gatech.edu"
+        }
+    else:
+        return {"string": "Creating user."}
+
+@app.route("/api/v1/user/photos", methods=["GET"])
+def getPhotos():
+    return {"string": "Getting photos."}
