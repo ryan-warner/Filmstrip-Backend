bb55355328358e114760286e5b9a07c4db28e173
Ryan Warner <ryan.warner.us@gmail.com>
2022-07-23 22:12:23 +0200

    Added album model

--- test.py
+++ test.py
@@ -7,13 +7,25 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Users(db.Model):
-    userId = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), unique=True, nullable=False)
-    email = db.Column(db.String(255), unique=True, nullable=False)
-    firstName = db.Column(db.String(255), nullable=False)
-    lastName = db.Column(db.String(255), nullable=False)
-    password = db.Column(db.BINARY(length=60), nullable=False)
-    needsNewToken = db.Column(db.BOOLEAN, default=False)
+    userID = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True)
+    email = db.Column(db.String(255), unique=True)
+    firstName = db.Column(db.String(255))
+    lastName = db.Column(db.String(255))
+    password = db.Column(db.BINARY(length=60))
+    needsNewToken = db.Column(db.Boolean, default=False)
     registrationMethod = db.Column(db.String(255), default="username")
 
-print(Users.query.filter_by(firstName='Ryan').first())
+class Albums(db.Model):
+    albumId = db.Column(db.Integer, primary_key=True)
+    albumName = db.Column(db.String(255))
+    userID = db.Column(db.Integer)
+    albumDescription = db.Column(db.String(255))
+    albumCamera = db.Column(db.String(255))
+    albumFormat = db.Column(db.String(255), default="35mm")
+    albumFilm = db.Column(db.String(255))
+
+for item in Users.query.all():
+    print(item.username)
+
+print(Albums.query.filter_by(albumName='Better Name :)').first().albumName)
