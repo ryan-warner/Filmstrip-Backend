b325a4483279b2cbeeedcf280b4e505735b1b0da
Ryan Warner <ryan.warner.us@gmail.com>
2022-07-25 11:23:30 +0200

    Cleaned up app.json

--- app.py
+++ app.py
@@ -12,20 +12,3 @@ db.init_app(app)
 app.register_blueprint(albumsBlueprint)
 app.register_blueprint(userBlueprint)
 app.register_blueprint(authBlueprint)
-
-testJson = {
-    "username": "test",
-    "firstName": "test",
-    "lastName": "test",
-    "email": "fake@gmail.com",
-}
-
-#testUser = Users(username="me3", email="rwarner322@gatech.edu", password=b'123')
-#db.session.add(testUser)
-#db.session.commit()
-
-#for item in Users.query.all():
-#    for album in item.albums:
-#        print(album.albumDescription)
-
-#print(Albums.query.filter_by(albumName='Better Name :)').first().albumName)
