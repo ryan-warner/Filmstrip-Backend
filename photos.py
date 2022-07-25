e59b3562ab8f4949c04058cb88b1b779d92be4ca
Ryan Warner <ryan.warner.us@gmail.com>
2022-07-25 15:09:42 +0200

    Simple thumbnail generation

--- photos.py
+++ photos.py
@@ -1,8 +1,9 @@
-from flask import request, Blueprint, jsonify, url_for
+from flask import request, Blueprint, jsonify, send_from_directory, url_for
 from database import Photos, Users, db
 from werkzeug.utils import secure_filename
 import validateToken
-from os import path
+from PIL import Image
+from os import path, remove
 
 from dotenv import dotenv_values
 config = dotenv_values(".env")
@@ -23,21 +24,36 @@ def photos(currentUser):
 
         if file and allowedFile(file.filename):
             filename = secure_filename(file.filename)
-            file.save(path.join(config["UPLOAD_FOLDER"], filename))
+            #file.save(path.join(config["UPLOAD_FOLDER"], filename))
+
+        createThumbnail(filename)
 
         photo = Photos(**request.form)
+        photo.photoName = filename.split(".")[0]
         photo.userID = currentUser.userID
         photo.photoPath = path.join(config["UPLOAD_FOLDER"], filename)
         photo.thumbPath = path.join(config["THUMB_FOLDER"], filename)
-        db.session.add(photo)
-        db.session.commit()
+        #db.session.add(photo)
+        #db.session.commit()
         return {"string": "Created photo."}
     elif request.method == "PATCH":
         return {"string": "Updating photo."}
     elif request.method == "GET":
-        return {"string": "Getting photo."}
+        photos = Users.query.filter_by(email=currentUser.email).first().photos
+        #print(photos[0].photoPath)
+        output = []
+        for photo in photos:
+
+            #print(photo.photoPath)
+            output.append(url_for("photosBlueprint.public", filename=photo.photoPath))
+            print(output[0])
+            
+            return {"string" : "hi"}#send_from_directory(config["UPLOAD_FOLDER"], photo.photoPath.split("/")[-1])
     elif request.method == "DELETE":
-        file.delete(path.join(config["UPLOAD_FOLDER"], filename))
+        photo = Photos.query.filter_by(photoID=request.json["photoID"]).first()
+        remove(photo.photoPath)
+        Photos.query.filter_by(photoID=request.json["photoID"]).delete()
+        db.session.commit()
         return {"string": "Deleted photo."}
 
 def allowedFile(filename):
@@ -47,6 +63,11 @@ def allowedFile(filename):
 def saveImage():
     return {"string": "Saving image."}
 
-def createThumbnail():
-    return {"string": "Creating thumbnail."}
-
+def createThumbnail(filename):
+    try:
+        image = Image.open(config["UPLOAD_FOLDER"] + "/" + filename)
+        size = 512, 512
+        image.thumbnail(size)
+        image.save(config["THUMB_FOLDER"] + "/" + filename)
+    except IOError:
+        return IOError
